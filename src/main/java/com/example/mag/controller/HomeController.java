19c68b87de7784ae0998a4a4ed3a4f83f23601e2
tonaycst-ux <tonay.cst@gmail.com>
2026-09-23 22:36:58 +0530

    Update HomeController.java

--- src/main/java/com/example/mag/controller/HomeController.java
+++ src/main/java/com/example/mag/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.mag.service.MessageService;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.List;
 
 @RestController
